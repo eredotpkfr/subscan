0d338557d2edd045123d5b30ee73f4ce409c470a
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2024-09-28 15:19:31 +0300

    test: add binaryedge module tests

---
+++ tests/modules/integrations/binaryedge_test.rs
@@ -0,0 +1,51 @@
+use crate::common::{
+    constants::{TEST_BAR_SUBDOMAIN, TEST_BAZ_SUBDOMAIN, TEST_DOMAIN},
+    mocks::wrap_url_with_mock_func,
+};
+use serde_json::{self, Value};
+use std::{collections::BTreeSet, env};
+use subscan::{
+    interfaces::module::SubscanModuleInterface,
+    modules::integrations::binaryedge::{self, BINARYEDGE_MODULE_NAME, BINARYEDGE_URL},
+};
+
+#[tokio::test]
+#[stubr::mock("module/integrations/binaryedge.json")]
+async fn binaryedge_run_test() {
+    env::set_var("SUBSCAN_BINARYEDGE_APIKEY", "binaryedge-api-key");
+
+    let mut binaryedge = binaryedge::Binaryedge::new();
+
+    binaryedge.url = wrap_url_with_mock_func(stubr.path("/binaryedge").as_str());
+
+    let result = binaryedge.run(TEST_DOMAIN.to_string()).await;
+
+    assert_eq!(binaryedge.name().await, BINARYEDGE_MODULE_NAME);
+    assert_eq!(
+        result,
+        [
+            TEST_BAR_SUBDOMAIN.to_string(),
+            TEST_BAZ_SUBDOMAIN.to_string(),
+        ]
+        .into()
+    );
+}
+
+#[tokio::test]
+async fn get_query_url_test() {
+    let url = binaryedge::Binaryedge::get_query_url(TEST_DOMAIN);
+    let expected = format!("{BINARYEDGE_URL}/{TEST_DOMAIN}");
+
+    assert_eq!(url, expected);
+}
+
+#[tokio::test]
+async fn extract_test() {
+    let json = "{\"events\": [\"bar.foo.com\"]}";
+
+    let extracted = binaryedge::Binaryedge::extract(serde_json::from_str(json).unwrap());
+    let not_extracted = binaryedge::Binaryedge::extract(Value::default());
+
+    assert_eq!(extracted, [TEST_BAR_SUBDOMAIN.to_string()].into());
+    assert_eq!(not_extracted, BTreeSet::new());
+}
