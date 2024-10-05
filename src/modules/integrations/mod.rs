1c5a1705fa7e80a4734317d9a41aa190b6f876d3
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2024-10-05 17:05:12 +0300

    feat: add hackertarget integration module

--- src/modules/integrations/mod.rs
+++ src/modules/integrations/mod.rs
@@ -19,5 +19,7 @@ pub mod certspotter;
 pub mod chaos;
 /// `Crt.sh` API integration
 pub mod crtsh;
-/// `Digitorus` API integration
+/// `Digitorus` HTML crawler integration
 pub mod digitorus;
+/// `HackerTarget` HTML crawler integration
+pub mod hackertarget;
