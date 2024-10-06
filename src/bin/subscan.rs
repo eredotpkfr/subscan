97b5c08220a63893b3c0527bf60eab8843f88265
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2024-10-06 13:56:18 +0300

    feat: add leakix api integration

--- src/bin/subscan.rs
+++ src/bin/subscan.rs
@@ -17,7 +17,7 @@ async fn main() {
         let mut module = item.lock().await;
         let requester = module.requester().await.unwrap();
 
-        if module.name().await != "hackertarget" {
+        if module.name().await != "leakix" {
             continue;
         }
 
