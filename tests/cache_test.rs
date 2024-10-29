6e00eb6719017305e77725fb5eba10185cff910a
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2024-10-29 23:49:58 +0300

    test: remove unnecessary module definition on cache tests, [skip ci]

--- tests/cache_test.rs
+++ tests/cache_test.rs
@@ -1,42 +1,39 @@
-#[cfg(test)]
-mod modules {
-    use std::time::Duration;
-    use subscan::{
-        cache::CacheManager,
-        interfaces::{module::SubscanModuleInterface, requester::RequesterInterface},
-        types::config::RequesterConfig,
+use std::time::Duration;
+use subscan::{
+    cache::CacheManager,
+    interfaces::{module::SubscanModuleInterface, requester::RequesterInterface},
+    types::config::RequesterConfig,
+};
+
+#[tokio::test]
+async fn configure_test() {
+    let manager = CacheManager::default();
+
+    let old_config = RequesterConfig::default();
+    let new_config = RequesterConfig {
+        timeout: Duration::from_secs(120),
+        ..Default::default()
     };
 
-    #[tokio::test]
-    async fn configure_test() {
-        let manager = CacheManager::default();
-
-        let old_config = RequesterConfig::default();
-        let new_config = RequesterConfig {
-            timeout: Duration::from_secs(120),
-            ..Default::default()
-        };
-
-        for module in manager.modules().await.iter() {
-            if let Some(requester) = module.lock().await.requester().await {
-                assert_eq!(requester.lock().await.config().await, &old_config);
-            }
-        }
-
-        manager.configure(new_config.clone()).await;
-
-        for module in manager.modules().await.iter() {
-            if let Some(requester) = module.lock().await.requester().await {
-                assert_eq!(requester.lock().await.config().await, &new_config);
-            }
+    for module in manager.modules().await.iter() {
+        if let Some(requester) = module.lock().await.requester().await {
+            assert_eq!(requester.lock().await.config().await, &old_config);
         }
     }
 
-    #[tokio::test]
-    async fn module_test() {
-        let manager = CacheManager::default();
+    manager.configure(new_config.clone()).await;
 
-        assert!(manager.module("foo").await.is_none());
-        assert!(manager.module("google").await.is_some());
+    for module in manager.modules().await.iter() {
+        if let Some(requester) = module.lock().await.requester().await {
+            assert_eq!(requester.lock().await.config().await, &new_config);
+        }
     }
+}
+
+#[tokio::test]
+async fn module_test() {
+    let manager = CacheManager::default();
+
+    assert!(manager.module("foo").await.is_none());
+    assert!(manager.module("google").await.is_some());
 }
