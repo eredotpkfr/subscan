1a57929f081fe6f7eb4266d406d64281553874bd
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2024-10-30 19:47:42 +0300

    chore: minor refactor on log utils, [skip ci]

--- src/utilities/log.rs
+++ src/utilities/log.rs
@@ -4,14 +4,7 @@ use colored::Colorize;
 /// Log any [`SubscanModuleResult`] object by its status
 pub async fn result(result: SubscanModuleResult) {
     match result.status {
-        SubscanModuleStatus::Started => {
-            log::info!(
-                "{:.<25}{:.>35}",
-                result.module.white(),
-                result.status.with_reason().await.white()
-            )
-        }
-        SubscanModuleStatus::Finished => {
+        SubscanModuleStatus::Started | SubscanModuleStatus::Finished => {
             log::info!(
                 "{:.<25}{:.>35}",
                 result.module.white(),
@@ -38,20 +31,15 @@ pub async fn result(result: SubscanModuleResult) {
 /// Log any status with any module
 pub async fn status(module: &str, status: SubscanModuleStatus) {
     match status {
+        SubscanModuleStatus::Started | SubscanModuleStatus::Finished => log::info!(
+            "{:.<25}{:.>35}",
+            module.white(),
+            status.with_reason().await.white()
+        ),
         SubscanModuleStatus::Skipped(_) => log::warn!(
             "{:.<25}{:.>35}",
             module.yellow(),
             status.with_reason().await.yellow()
-        ),
-        SubscanModuleStatus::Started => log::info!(
-            "{:.<25}{:.>35}",
-            module.white(),
-            status.with_reason().await.white()
-        ),
-        SubscanModuleStatus::Finished => log::info!(
-            "{:.<25}{:.>35}",
-            module.white(),
-            status.with_reason().await.white()
         ),
         SubscanModuleStatus::Failed(_) => log::error!(
             "{:.<25}{:.>35}",
