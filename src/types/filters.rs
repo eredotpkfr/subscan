3af4878d86eba4a775524443cac4597c09a744fe
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2024-11-01 16:51:35 +0300

    test: add doctests

--- src/types/filters.rs
+++ src/types/filters.rs
@@ -5,6 +5,18 @@ pub struct ModuleNameFilter {
 }
 
 impl From<(Vec<String>, Vec<String>)> for ModuleNameFilter {
+    /// Create [`ModuleNameFilter`] from tuple
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use subscan::types::filters::ModuleNameFilter;
+    ///
+    /// let filter = ModuleNameFilter::from((vec![], vec!["foo".into()]));
+    ///
+    /// assert!(filter.valids.is_empty());
+    /// assert!(!filter.invalids.is_empty());
+    /// ```
     fn from(tuple: (Vec<String>, Vec<String>)) -> Self {
         Self {
             valids: tuple.0,
--- src/utilities/serializers.rs
+++ src/utilities/serializers.rs
@@ -2,6 +2,20 @@ use chrono::{DateTime, TimeDelta, Utc};
 use serde::Serializer;
 
 /// Serializer method to convert [`DateTime`] objects to strings
+///
+/// # Examples
+///
+/// ```
+/// use serde_json::json;
+/// use chrono::DateTime;
+///
+/// let json = json!({
+///     "time": DateTime::UNIX_EPOCH.to_string(),
+/// });
+/// let expected = "{\"time\":\"1970-01-01 00:00:00 UTC\"}";
+///
+/// assert_eq!(serde_json::to_string(&json).unwrap(), expected);
+/// ```
 pub fn dt_to_string_method<S>(dt: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
@@ -10,6 +24,20 @@ where
 }
 
 /// Serializer method to convert [`TimeDelta`] objects to [`i64`] seconds
+///
+/// # Examples
+///
+/// ```
+/// use serde_json::json;
+/// use chrono::TimeDelta;
+///
+/// let json = json!({
+///     "seconds": TimeDelta::zero().num_seconds(),
+/// });
+/// let expected = "{\"seconds\":0}";
+///
+/// assert_eq!(serde_json::to_string(&json).unwrap(), expected);
+/// ```
 pub fn td_num_seconds_method<S>(td: &TimeDelta, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
