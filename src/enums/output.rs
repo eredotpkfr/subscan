57a5d5ed1a7fdb3f47ff7bdac9331cf6a1c9235d
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2024-11-06 21:10:42 +0300

    chore: remove unneeded impl, [skip ci]

--- src/enums/output.rs
+++ src/enums/output.rs
@@ -1,5 +1,4 @@
 use clap::ValueEnum;
-use std::fmt;
 
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 pub enum OutputFormat {
@@ -7,14 +6,4 @@ pub enum OutputFormat {
     CSV,
     #[default]
     JSON,
-}
-
-impl fmt::Display for OutputFormat {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            OutputFormat::TXT => write!(f, "TXT"),
-            OutputFormat::CSV => write!(f, "CSV"),
-            OutputFormat::JSON => write!(f, "JSON"),
-        }
-    }
 }
