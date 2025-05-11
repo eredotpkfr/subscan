e81980216a19eeb4c700b4fdb971f497fe1e81bb
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2025-05-11 12:38:54 +0300

    docs: update project book

--- src/cli/commands/brute.rs
+++ src/cli/commands/brute.rs
@@ -19,8 +19,8 @@ pub struct BruteCommandArgs {
     /// If sets, output will be logged on stdout
     #[arg(long, default_value_t = false)]
     pub print: bool,
-    /// Optional TXT file to create file stream for the subdomains that found.
-    /// The `--output` parameter will be disabled
+    /// Optional txt file to create file stream for the subdomains that found.
+    /// If sets the `--output` parameter will be disabled
     #[arg(short, long, default_value = None)]
     pub stream_to_txt: Option<PathBuf>,
     /// Set output format
