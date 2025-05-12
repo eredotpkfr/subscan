8da8035f7ee2e8696194e24f0f725a4f61eaeaac
Erdogan Yoksul <erdogan.yoksul@gusto.com>
2025-05-12 21:30:52 +0300

    test: update zonetransfer test cases

--- tests/components/modules/zonetransfer_test.rs
+++ tests/components/modules/zonetransfer_test.rs
@@ -1,4 +1,8 @@
-use std::{collections::BTreeSet, net::SocketAddr, str::FromStr};
+use std::{
+    collections::BTreeSet,
+    net::{SocketAddr, SocketAddrV4},
+    str::FromStr,
+};
 
 use hickory_client::proto::xfer::Protocol;
 use hickory_resolver::config::NameServerConfig;
@@ -58,7 +62,22 @@ async fn attempt_zone_transfer_test() {
 }
 
 #[tokio::test]
-async fn run_failed_test() {
+async fn run_success_test() {
+    let server = funcs::spawn_mock_dns_server().await;
+    let mut zonetransfer = ZoneTransfer::dispatcher();
+
+    if let SubscanModuleDispatcher::ZoneTransfer(ref mut zonetransfer) = zonetransfer {
+        zonetransfer.ns = Some(NameServerConfig::new(server.socket, Protocol::Tcp));
+    }
+
+    let (results, status) = utils::run_module(zonetransfer, TEST_DOMAIN).await;
+
+    assert_eq!(results, [TEST_BAR_SUBDOMAIN.into()].into());
+    assert_eq!(status, SubscanModuleStatus::Finished);
+}
+
+#[tokio::test]
+async fn run_no_default_ns_test() {
     let mut zonetransfer = ZoneTransfer::dispatcher();
 
     if let SubscanModuleDispatcher::ZoneTransfer(ref mut zonetransfer) = zonetransfer {
@@ -72,16 +91,16 @@ async fn run_failed_test() {
 }
 
 #[tokio::test]
-async fn run_test() {
-    let server = funcs::spawn_mock_dns_server().await;
+async fn run_failed_test() {
     let mut zonetransfer = ZoneTransfer::dispatcher();
+    let socketaddr = SocketAddr::V4(SocketAddrV4::from_str("0.0.0.0:0").unwrap());
 
     if let SubscanModuleDispatcher::ZoneTransfer(ref mut zonetransfer) = zonetransfer {
-        zonetransfer.ns = Some(NameServerConfig::new(server.socket, Protocol::Tcp));
+        zonetransfer.ns = Some(NameServerConfig::new(socketaddr, Protocol::Tcp));
     }
 
     let (results, status) = utils::run_module(zonetransfer, TEST_DOMAIN).await;
 
-    assert_eq!(results, [TEST_BAR_SUBDOMAIN.into()].into());
-    assert_eq!(status, SubscanModuleStatus::Finished);
+    assert_eq!(results, BTreeSet::new());
+    assert_eq!(status, "connection error".into())
 }
